c21573d3e5f80309b167ce4c6e80da95d6257958
jokerlin <anonymous.joker.lin@gmail.com>
2022-06-16 10:35:51 +0800

    continue init config

--- init.lua
+++ init.lua
@@ -3,3 +3,11 @@ package.path = "/Users/jokerlin/.config/nvim/?.lua;"
 require("basic")
 -- 快捷键映射
 require("keybindings")
+-- Packer插件管理
+require("plugins")
+-- 主题设置
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
